a7906c2d65638cedf1e2fcd1462c189bf698c9c7
Koldo Abasolo Iturregi <kabasolo@c3r8s1.42urduliz.com>
2024-10-16 12:51:48 +0200

    poniendo bonito

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 12:33:21 by kabasolo          #+#    #+#             */
-/*   Updated: 2024/10/16 12:36:16 by kabasolo         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:51:18 by kabasolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,25 @@
 // --- // --- // --- // --- // --- //
 // Funciones para la inicializacion de la estructura
 // --- // --- // --- // --- // --- //
-int						init_data_bresenham(t_bresenham_vars *vars, 
-							t_line_params *params);
-int						init_data_all(t_game *game,
-							const char *direction_input);
-int						init_data_map(t_game *game);
-int						init_data_player(t_game *game,
-							const char *directionInput);
-int						init_data_rays(t_game *game);
-int						init_data_figures(t_game *game);
-int						init_data_render(t_game *game, int i);
+int		init_data_bresenham(t_bresenham_vars *vars, t_line_params *params);
+int		init_data_all(t_game *game, const char *direction_input);
+int		init_data_map(t_game *game);
+int		init_data_player(t_game *game, const char *directionInput);
+int		init_data_rays(t_game *game);
+int		init_data_figures(t_game *game);
+int		init_data_render(t_game *game, int i);
 
 // --- // --- // --- // --- // --- //
 
 // Funciones para setear la dirección del jugador
 
 // --- // --- // --- // --- // --- //
-int						set_player_direction(t_player *player,
-							const char *directionInput);
+int		set_player_direction(t_player *player, const char *directionInput);
 // Añadido
 
 // --- // --- // --- // --- // --- //
 // Function for update postion of player position
-int						move_player(t_game *game, float deltaTime);
+int		move_player(t_game *game, float deltaTime);
 // --- // --- // --- // --- // --- //
 
 // --- // --- // --- // --- // --- //
@@ -51,22 +47,15 @@ int						move_player(t_game *game, float deltaTime);
 // --- // --- // --- // --- // --- // // --- // --- // --- // --- // --- //
 // Function for draw a rectangle form in mlx
 
-// --- // --- // --- // --- // --- //
+/* --- // --- // --- // --- // --- //
+	Funcion para dibujar una linea recta en mlx con base en el algoritmo de bresenham
+// --- // --- // --- // --- // --- */
+int		draw_line(t_game *game);
 
-	/* 
-	Funcion para dibujar una linea recta en mlx con base 
-	en el algoritmo de bresenham
-	*/
-// --- // --- // --- // --- // --- //
-int draw_line(t_game *game);
-
-// --- // --- // --- // --- // --- //
-	/*
+/* --- // --- // --- // --- // --- //
 	Funcion para dibujar un rectangulo en mlx
-	*/
-
-// --- // --- // --- // --- // --- //
-int draw_rectangle(t_game *game);
+// --- // --- // --- // --- // --- */
+int		draw_rectangle(t_game *game);
 
 
 // void					draw_rectangle(mlx_image_t *img, int x, int y,
@@ -76,12 +65,12 @@ int draw_rectangle(t_game *game);
 // void					draw_line(mlx_image_t *img, int x0, int y0, int x1,
 // 							int y1, uint32_t color);
 
-void					destroy_window(t_game *game);
+void	destroy_window(t_game *game);
 
-void					key_hook(mlx_key_data_t keydata, void *param);
+void	key_hook(mlx_key_data_t keydata, void *param);
 
 // -- // -- // --- // -- //
-int						map_has_wall_at(t_game *game, float x, float y);
+int		map_has_wall_at(t_game *game, float x, float y);
 // --- // --- // --- // --- // --- //
 // Funciones para determinar la poscionde del rayo en un plano 2D
 // Ademas de Funciones auxiliares para realizar el calculo
@@ -92,31 +81,26 @@ int						map_has_wall_at(t_game *game, float x, float y);
 
 // void					cast_ray(t_game *game, float rayAngle, int stripid);
 
-int	initialize_ray(t_game *game, float *rayAngle, int stripid);
-int	calculate_hit_distances(t_game *game, t_ray_cast *ray_cast, int stripid);
-int	assign_ray_properties(t_game *game, float rayAngle, int stripid);
+int		initialize_ray(t_game *game, float *rayAngle, int stripid);
+int		calculate_hit_distances(t_game *game, t_ray_cast *ray_cast, int stripid);
+int		assign_ray_properties(t_game *game, float rayAngle, int stripid);
 
-void					cast_all_rays(t_game *game);
-int	cast_ray_setup(t_game *game, float rayAngle, int stripid);
-int	find_horizontal_hit(t_game *game, t_ray_cast *ray_cast, t_player *player, float rayAngle);
-int	find_vertical_hit(t_game *game, t_ray_cast *ray_cast,
-		t_player *player, float rayAngle);
-
-
-
+void	cast_all_rays(t_game *game);
+int		cast_ray_setup(t_game *game, float rayAngle, int stripid);
+int		find_horizontal_hit(t_game *game, t_ray_cast *ray_cast, t_player *player, float rayAngle);
+int		find_vertical_hit(t_game *game, t_ray_cast *ray_cast, t_player *player, float rayAngle);
 
 // --- // --- // --- // --- // --- //
 
-void					render_rays(t_game *game);
-void					render_player(t_game *game);
-int						render_map(t_game *game);
-void					update(void *param);
-int						setup(t_game *game, const char *direction_input);
-float					normalize_angle(float angle);
-float					distance_between_points(float x1, float y1, float x2,
-							float y2);
+void	render_rays(t_game *game);
+void	render_player(t_game *game);
+int		render_map(t_game *game);
+void	update(void *param);
+int		setup(t_game *game, const char *direction_input);
+float	normalize_angle(float angle);
+float	distance_between_points(float x1, float y1, float x2, float y2);
 
-void					render_3d_projection(t_game *game);
+void	render_3d_projection(t_game *game);
 
 // Funciones para agres sprites y texturas al juego //
 
